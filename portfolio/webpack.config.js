c4de3f4dc1c5571d96a34f50662c89f53101728e
Bryan <bryangrigorie@gmail.com>
2024-09-06 18:56:29 -0400

    New bundle location

--- portfolio/webpack.config.js
+++ portfolio/webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
     entry: './assets/index.js', // Path to input file
     output: {
         filename: 'index-bundle.js', // Output bundle name
-        path: path.resolve(__dirname, './static/webpack'), // Path to static directory
+        path: path.resolve(__dirname, './static/js/bundles'), // Path to static directory
     },
 }
