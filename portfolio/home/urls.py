08c91d36f0ed6a9e158569ff38f134111d4eb0cc
Bryan <bryangrigorie@gmail.com>
2024-09-09 15:49:16 -0400

    fix paths

--- portfolio/home/urls.py
+++ portfolio/home/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('old/', views.index, name='index'),
     #path('skills', views.SkillsView.as_view(), name='skills'),
     #path('contact', views.ContactView.as_view(), name='contact'),
     #path('projects', views.ProjectsView.as_view(), name='projects'),
-    path('blog', views.blog, name='blog')
+    path('old/blog', views.blog, name='blog')
 ]
