eb4e67bf8cbe00592f37607781b5a7aef208960a
Bryan <bryangrigorie@gmail.com>
2024-09-12 21:25:45 -0400

    Bootstrapping projects and blog

--- portfolio/home/urls.py
+++ portfolio/home/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     #path('skills', views.SkillsView.as_view(), name='skills'),
     #path('contact', views.ContactView.as_view(), name='contact'),
-    #path('projects', views.ProjectsView.as_view(), name='projects'),
+    path('projects', views.projects, name='projects'),
     path('blog/', views.blog, name='blog')
 ]
--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -3,20 +3,32 @@ from .models import BlogPost, Skill, Project
 
 
 def index(request):
-    technical_skills = Skill.objects.filter(skill_category__category_name='Technical').order_by('-skill_level')
-    communication_skills = Skill.objects.filter(skill_category__category_name='Communication').order_by('-skill_level')
-    miscellaneous_skills = Skill.objects.filter(skill_category__category_name='Miscellaneous').order_by('-skill_level')
+    # technical_skills = Skill.objects.filter(skill_category__category_name='Technical').order_by('-skill_level')
+    # communication_skills = Skill.objects.filter(skill_category__category_name='Communication').order_by('-skill_level')
+    # miscellaneous_skills = Skill.objects.filter(skill_category__category_name='Miscellaneous').order_by('-skill_level')
 
+    # projects = Project.objects.all().order_by('-date')
+
+    # context = {
+    #     'technical_skills': technical_skills,
+    #     'communication_skills': communication_skills,
+    #     'miscellaneous_skills': miscellaneous_skills,
+    #     'projects': projects,
+    # }
+
+    context = {}
+
+    return render(request, 'home/index.html', context=context)
+
+
+def projects(request):
     projects = Project.objects.all().order_by('-date')
 
     context = {
-        'technical_skills': technical_skills,
-        'communication_skills': communication_skills,
-        'miscellaneous_skills': miscellaneous_skills,
         'projects': projects,
     }
 
-    return render(request, 'home/index.html', context=context)
+    return render(request, 'home/projects.html', context=context)
 
 
 def blog(request):
