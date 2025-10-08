e35b5f800f5257bffed431ff82ddf598586744a6
Bryan <bryangrigorie@gmail.com>
2025-10-08 11:15:31 -0400

    turnstile on login

--- users/context_processors.py
+++ users/context_processors.py
@@ -2,6 +2,7 @@ from django.conf import settings
 
 def global_settings(request):
     return {
-        'oidc_configured': getattr(settings, 'OIDC_CONFIGURED', False),
-        'oidc_op_display_name': getattr(settings, 'OIDC_OP_DISPLAY_NAME', None) or 'SSO',
+        'OIDC_CONFIGURED': getattr(settings, 'OIDC_CONFIGURED', False),
+        'OIDC_OP_DISPLAY_NAME': getattr(settings, 'OIDC_OP_DISPLAY_NAME', None) or 'SSO',
+        'TURNSTILE_SITE_KEY': getattr(settings, 'TURNSTILE_SITE_KEY', None),
     }
--- users/views.py
+++ users/views.py
@@ -4,11 +4,27 @@ from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.conf import settings
 
+from core.utils.turnstile_utils import validate_turnstile
+
 def login(request):
     if request.user.is_authenticated:
         return redirect(reverse('home:index'))
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
+        # Validate Turnstile token
+        if settings.TURNSTILE_CONFIGURED:
+            token = request.POST.get('cf-turnstile-response')
+            if not token:
+                form.add_error(None, "Security challenge failed. Please refresh and try again.")
+                return render(request, 'users/login.html', {'form': form})
+            remoteip = request.headers.get('CF-Connecting-IP') or \
+                request.headers.get('X-Forwarded-For') or \
+                request.META.get('REMOTE_ADDR')
+            cf_validation = validate_turnstile(token, settings.TURNSTILE_SECRET_KEY, remoteip)
+            if cf_validation['success'] == False:
+                form.add_error(None, "Security challenge failed. Please refresh and try again.")
+                return render(request, 'users/login.html', {'form': form})
+        # Validate credentials
         if form.is_valid():
             user = form.get_user()
             auth_login(request, user)
