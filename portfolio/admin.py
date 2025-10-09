861dbac9c700ea85c4cd335dd4e60e1e3a24ded9
Bryan <bryangrigorie@gmail.com>
2025-10-09 14:54:48 -0400

    config.brand in admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import reverse
+from constance import config
 
 class CustomAdminSite(admin.AdminSite):
     """
     https://docs.djangoproject.com/en/5.2/ref/contrib/admin/#customizing-the-adminsite-class
     https://docs.djangoproject.com/en/5.2/ref/contrib/admin/#overriding-the-default-admin-site
     """
-    site_header = site_title = 'Portfolio site administration'
+    site_header = site_title = f'{config.get('BRAND')} Admin'
+    index_title = 'Admin interface'
 
     def login(self, request, extra_context=None):
         return redirect(reverse('login'))
