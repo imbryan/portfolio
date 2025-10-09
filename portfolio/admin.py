2bed316ef2b37945fad3c83a820856fd894c33b3
Bryan <bryangrigorie@gmail.com>
2025-10-09 14:59:23 -0400

    admin config.brand fix

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -8,7 +8,7 @@ class CustomAdminSite(admin.AdminSite):
     https://docs.djangoproject.com/en/5.2/ref/contrib/admin/#customizing-the-adminsite-class
     https://docs.djangoproject.com/en/5.2/ref/contrib/admin/#overriding-the-default-admin-site
     """
-    site_header = site_title = f'{config.get('BRAND')} Admin'
+    site_header = site_title = f'{getattr(config, 'BRAND')} Admin'
     index_title = 'Admin interface'
 
     def login(self, request, extra_context=None):
