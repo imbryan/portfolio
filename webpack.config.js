b58c5f149c69aded83eab4cdb5375628e127c7d1
Bryan Grigorie <bryangrigorie@gmail.com>
2025-11-03 14:51:34 -0500

    js package cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,6 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
-            },
-            {
                 test: /\.scss$/,
                 use: [
                     isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
