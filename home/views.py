65451b1426cfac02d852103f84bda797e61fc9af
Bryan Grigorie <bryangrigorie@gmail.com>
2025-11-05 14:04:46 -0500

    card changes

--- home/views.py
+++ home/views.py
@@ -25,7 +25,7 @@ def index(request):
     certs = Certification.objects.filter(hidden=False).order_by('-date_issued')
     experiences = Experience.objects.filter(hidden=False).order_by('-start_date')
 
-    latest_blog_posts = BlogPost.objects.filter(about_content=False, hidden=False).order_by('-date_published').all()[:3]
+    latest_blog_posts = BlogPost.objects.filter(about_content=False, hidden=False).order_by('-date_published').all()[:5]
 
     context = {
         'educations': educations,
