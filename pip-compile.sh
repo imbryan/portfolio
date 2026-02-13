7b757173ade9cf4a3936cbba3dfba776287cbe93
Bryan Grigorie <bryangrigorie@gmail.com>
2026-02-12 20:06:17 -0500

    chore: bump Pillow, cryptography, webpack

--- pip-compile.sh
+++ pip-compile.sh
@@ -1,2 +1,2 @@
-pip-compile pyproject.toml --output-file requirements.txt
-pip-compile pyproject.toml --output-file requirements.dev.txt --extra dev
+pip-compile pyproject.toml --output-file requirements.txt --constraint constraints.txt
+pip-compile pyproject.toml --output-file requirements.dev.txt --extra dev --constraint constraints.txt
